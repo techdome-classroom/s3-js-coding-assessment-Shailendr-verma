39786cb57f2efe3899038ca36efc90f2f78d323a
Shailendra verma <shailendra1630verma@gmail.com>
2024-09-25 15:29:57 +0530

    done

--- program1.js
+++ program1.js
@@ -2,10 +2,28 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
-};
 
-module.exports = { isValid };
-
-
+function isValid(s) {
+    const stack = [];
+    const mapping = {
+      ')': '(',
+      '}': '{',
+      ']': '['
+    };
+  
+    for (let i = 0; i < s.length; i++) {
+      const char = s[i];
+  
+      if (char === '(' || char === '{' || char === '[') {
+        stack.push(char);
+      } else if (char === ')' || char === '}' || char === ']') {
+        if (stack.length === 0 || mapping[char] !== stack.pop()) {
+          return false;
+        }
+      }
+    }
+  
+    return stack.length === 0;
+  }
+  
+  module.exports = { isValid };
--- program2.js
+++ program2.js
@@ -2,9 +2,31 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-    
-};
 
-
-module.exports={romanToInt}
+function romanToInt(s) {
+    const romanMap = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
+    };
+  
+    let result = 0;
+    for (let i = 0; i < s.length; i++) {
+      const current = romanMap[s[i]];
+      const next = romanMap[s[i + 1]];
+  
+      if (next && current < next) {
+        result -= current;
+      } else {
+        result += current;
+      }
+    }
+  
+    return result;
+  }
+  
+  module.exports = { romanToInt };
